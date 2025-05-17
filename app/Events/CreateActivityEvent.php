9de10c5276513efe667d88821a3a06611ce45cf6
NicollasMatheus37 <nicollasmatheus37@gmail.com>
2025-05-17 17:29:57 -0300

    feat: added activity events

---
+++ app/Events/CreateActivityEvent.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Activity;
+
+class CreateActivityEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(
+        public int $roomId,
+        public string $nickname,
+        public string $activity,
+    )
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('activity'),
+        ];
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'activity.created';
+    }
+}
