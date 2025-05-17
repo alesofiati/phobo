a3690901b96708e37ede98e7fd7af2701f454b3d
Alexandre Soffiatti <alexandre@uex.io>
2025-05-17 16:36:13 -0300

    feat: adiciona index na coluna nick_name na tabela de users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('nick_name')->unique();
             $table->timestamps();
+            $table->index('nick_name');
         });
     }
 
