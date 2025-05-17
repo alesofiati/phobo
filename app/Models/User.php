0cabdb9817228a0e8b134977844beb7c2772b4d4
Alexandre Soffiatti <alexandresofiati@hotmail.com>
2025-05-17 15:17:08 -0300

    feat: crud usuarios (#4)
    * feat: adiciona laravel api
    
    * changed: muda migração de usuarios para conter apenas nick_name e expired_at, aplica mudanças na model e factory para conter os mesmo campos
    
    * feat: adicionado estrutura de rotas para usuarios
    
    * delete: remove campo de expired_at da model, factory e migração
    
    * feat: adiciona pest para pipe de tests
    
    * feat: adiciona arquivo de configuração do pest
    
    * feat: adiciona refresh database nos tests
    
    * feat: adiciona configuração de ambiente para banco de test
    
    * feat: cria um novo usuario
    
    * feat: Adicionado traduções do backend
    
    * feat: Adicionado rota para verificar se o nick name do usuario esta disponivel
    
    * fix: corrige chamada da função para gerar nick name único
    
    ---------
    
    Co-authored-by: Alexandre Soffiatti <alexandre@uex.io>

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
+    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
     /**
@@ -18,31 +19,11 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'nick_name',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    public function scopeByNickName($query, string $nickName)
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $query->where('nick_name', $nickName);
     }
 }
---
+++ lang/pt-br/messages.php
@@ -0,0 +1,6 @@
+<?php
+
+return[
+    'nick_name_already_exists' => 'O apelido já existente.',
+    'nick_name_available' => 'O apelido está disponível.',
+];
