f2b10abc1f02c8a30fd25293d316fe8b6ee9250e
NicollasMatheus37 <nicollasmatheus37@gmail.com>
2025-05-18 01:09:35 -0300

    feat: Crud de atividades
    - Criada tabela de atividades
    - Criado model de atividades
    - Criado enum de tipos de atividades

---
+++ database/migrations/2025_05_18_033458_create_activities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('room_id')->constrained();
+            $table->string('type');
+            $table->integer('rating')->nullable();
+            $table->string('description')->nullable();
+            $table->string('image')->nullable();
+            $table->integer('episode')->nullable();
+            $table->integer('season')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activities');
+    }
+};
