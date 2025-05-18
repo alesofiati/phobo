30467840364ff5385ba3e55565f2a63ffc80d6ee
Alexandre Soffiatti <alexandre@uex.io>
2025-05-18 01:44:33 -0300

    fix: remove foreign key constraint on room_id in users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('room_id')->constrained();
             $table->string('nick_name')->unique();
             $table->timestamps();
             $table->index('nick_name');
