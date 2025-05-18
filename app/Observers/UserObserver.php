1ae231b24521fac0d3b685be3f70194cf14fde07
NicollasMatheus37 <nicollasmatheus37@gmail.com>
2025-05-18 00:57:18 -0300

    feat: Observer de criação do usuário por convite
    - Criação da atividade por meio de observer após o usuário ser criado
    - Adicionado campo room_id na tabela de users

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Observers;
+
+use App\ActivityTypeEnum;
+use App\Models\User;
+
+class UserObserver
+{
+    public function created(User $user): void
+    {
+        if (!$user->room_id) {
+            return;
+        }
+
+        $user->activities()->create([
+            'room_id' => $user->id,
+            'type' => ActivityTypeEnum::USER_INVITED->value,
+        ]);
+    }
+}
